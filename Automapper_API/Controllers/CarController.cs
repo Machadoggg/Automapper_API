b90f1a9ab18f902915bb086c9bcbf865e8c6ff90
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-22 21:31:58 -0500

    Add controller using mapper

---
+++ Automapper_API/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Automapper_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Automapper_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public CarController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Add(CarRequest request)
+        { 
+            Car car = _mapper.Map<Car>(request);
+
+            return Ok(car);
+        }
+
+    }
+}
