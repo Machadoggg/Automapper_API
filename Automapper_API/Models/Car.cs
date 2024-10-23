c7e6c0e0357801758ef5bece90e45e21cab3fe1a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-22 19:34:20 -0500

    Add models

---
+++ Automapper_API/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Automapper_API.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; } = default!;
+        public int Power { get; set; }
+        public string Model { get; set; } = default!;
+        public string Color { get; set; } = default!;
+    }
+}
---
+++ Automapper_API/Models/CarRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Automapper_API.Models
+{
+    public class CarRequest
+    {
+        public string Brand { get; set; } = default!;
+        public int Power { get; set; }
+    }
+}
