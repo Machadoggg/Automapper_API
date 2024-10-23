4308620f0b6dffb6582e9aa9490c7c2ba67470f7
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-22 19:54:19 -0500

    Add class MappingProfile

---
+++ Automapper_API/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Automapper_API.Models;
+
+namespace Automapper_API
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CarRequest, Car>();
+        }
+    }
+}
