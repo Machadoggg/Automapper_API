6e382a47ba0eea4495820453a98ae8d172197a54
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-22 21:38:34 -0500

    Add MappingProfile in program last step

--- Automapper_API/Program.cs
+++ Automapper_API/Program.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Automapper_API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 //mapper
 var mapperConfig = new MapperConfiguration(m =>
 {
+    m.AddProfile(new MappingProfile());
 });
 
 IMapper mapper = mapperConfig.CreateMapper();
